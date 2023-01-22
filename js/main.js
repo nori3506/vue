6806fa6e26bb7ea6c64ad03575084987d9a606fc
Nori <loveandfight2186@yahoo.co.jp>
2023-01-22 12:31:44 -0800

    implement addItem function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
   data: () => ({
-    
+    newItem: '',
+    todos: []
   }),
   methods: {
     addItem: function(event) {
-      // console.log('Clicked')
+      if (this.newItem === '') return
+
+      let todo = {
+        item: this.newItem,
+        isDone: false
+      }
+
+      this.todos.push(todo)
+      this.newItem = ''
     }
   }
 })
+
 app.mount('#app')
